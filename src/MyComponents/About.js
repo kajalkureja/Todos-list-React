8ee2548cd5cdd2b0a85ee712f7bd4743a412ded9
kajal kukreja <gudukukreja16@gmail.com>
2021-07-19 16:15:58 +0530

    todo app created
    you can add your todo list and delete it when you do your work. made using react

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+           This is an about component
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos officiis beatae similique asperiores rerum adipisci molestiae nam provident. Libero vel necessitatibus quaerat consequatur distinctio incidunt dignissimos facere magni porro, quos minus nisi qui tempore.</p> 
+        </div>
+    )
+}
